7a81f2a0908feeb06def6ceca5c716a36d29186e
manish <mgupta@es.iitr.ac.in>
2021-05-11 15:38:56 +0530

    second commit

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -7,4 +7,5 @@ import {expenses} from '../fixtures(dummy).js/fixtures';
 test('should render Expense List Item',()=>{
     const wrapper=shallow(<ExpenseListItem {...expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
+    console.log('happy me');
 })
